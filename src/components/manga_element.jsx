cf8222572c7d1efec083466b6bc9903ffa8fa5a2
eliasstein <elusteinkamp@hotmail.com>
2025-02-18 03:08:16 -0300

    Actualización MangaComponent
    Se actualizo el MangaComponent para que ahora si el titulo tiene mas 35 caracteres lo recorte y de esa manera no se desajuste el tamaño

--- src/components/manga_element.jsx
+++ src/components/manga_element.jsx
@@ -1,16 +1,23 @@
 import "../static/css/manga.css"
 import { useNavigate } from 'react-router-dom';
+import { useEffect,useState} from "react";
 
 export const MangaComponent= ({id,title,price,url}) =>{
     const navigate=useNavigate();
+    const [shortTitle,setShortTitle]=useState(title)
+
+    useEffect(() => {        
+        title.length>35?setShortTitle(title.slice(0,32)+"..."):setShortTitle(title)
+    });
+
 
 return(
 <div className="manga-container">
     <div className="manga-image-container">
     <img src={url} onClick={()=>navigate("/product?id="+id)}></img>
     </div>
-    <div>
-        <p>{title}</p>
+    <div className="manga-title-price">
+        <p className="manga-title">{shortTitle}</p>
         <p>{price}</p>
         <button className="cart-button"><i className="uil uil-shopping-cart"> </i>Añadir al carrito</button>
     </div>
